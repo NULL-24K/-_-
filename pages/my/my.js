31b8b93cd36f58967719dac3701894332bc9a272
NULL-24K <1182513617@qq.com>
2018-07-12 08:32:25 +0800

    修改样式

--- pages/my/my.js
+++ pages/my/my.js
@@ -27,7 +27,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
+    var that = this;23
     if (app.globalData.userInfo){
       var userinfo = app.globalData.userInfo
       if (userinfo.avatarUrl){
